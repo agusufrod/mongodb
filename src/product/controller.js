4bc54c03fea99f17b50d13bd1042332d68c29feb
agus ufrod <lkelinci0@gmail.com>
2022-12-02 20:10:46 +0700

    mongodb

---
+++ src/product/controller.js
@@ -0,0 +1,63 @@
+const { ObjectId } = require('mongodb');
+const db = require('../config/mongodb');
+
+const productController = {
+  getAllProductWithQuery: (req, res) => {
+    // const { name } = req.query;
+    db.collection('product')
+      .find()
+      .toArray()
+      .then((result) => res.send(result))
+      .catch((error) => res.send(error));
+  },
+
+  getDetailProduct: (req, res) => {
+    const { id } = req.params;
+    db.collection('product')
+      .findOne({ _id: ObjectId(id) })
+      .then((result) => res.send(result))
+      .catch((error) => res.send(error));
+  },
+
+  CreateProduct: (req, res) => {
+    const { name, stock, price, status } = req.body;
+    db.collection('product')
+      .insertOne({ name, stock, price, status })
+      .then((result) =>
+        res.send({
+          message: 'berhasil menambah data',
+          result,
+        })
+      )
+      .catch((error) => res.send(error));
+  },
+
+  UpdateProduct: (req, res) => {
+    const { id } = req.params;
+    const { name, stock, price, status } = req.body;
+    db.collection('product')
+      .updateOne({ _id: ObjectId(id) }, { $set: { name, stock, price, status } })
+      .then((result) =>
+        res.send({
+          message: 'berhasil update data',
+          result,
+        })
+      )
+      .catch((error) => res.send(error));
+  },
+
+  DeleteProduct: (req, res) => {
+    const { id } = req.params;
+    db.collection('product')
+      .deleteOne({ _id: ObjectId(id) })
+      .then((result) =>
+        res.send({
+          message: 'berhasil menghapus data',
+          result,
+        })
+      )
+      .catch((error) => res.send(error));
+  },
+};
+
+module.exports = productController;
